a8fdaa5538f4255a42423501a45e50ed2bd1ed7a
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-04-20 18:10:41 +0200

    Implemented the functionality for starting services.

---
+++ index.js
@@ -0,0 +1,17 @@
+const serviceConfigs = require('./services.json')
+const serviceRegistry = {}
+
+function startService(service) {
+    const serviceConfig = serviceConfigs[service]
+
+    if (serviceConfig.hasOwnProperty('dependencies'))
+        for (const dependency of serviceConfig.dependencies)
+            if (!serviceRegistry.hasOwnProperty(dependency))
+                startService(dependency)
+
+    serviceRegistry[service] = require(serviceConfig.path)(serviceRegistry)
+}
+
+for (const service in serviceConfigs)
+    if (serviceConfigs.hasOwnProperty(service) && !serviceRegistry.hasOwnProperty(service))
+        startService(service)
